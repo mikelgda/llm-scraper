e5c5181c32e94f65b4d5ea8750851e6ab9b85be9
mikelgda <mikel.gdaa@gmail.com>
2026-07-14 15:26:18 +0200

    remove credentials (correctly revoked)

--- settings.py
+++ settings.py
@@ -96,5 +96,5 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
-MONGO_URI = "mongodb+srv://mikelgdaa:panymantequilla@cluster0.qg4vt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-MONGO_DATABASE = "jobs"
+MONGO_URI = "????"
+MONGO_DATABASE = "????"
